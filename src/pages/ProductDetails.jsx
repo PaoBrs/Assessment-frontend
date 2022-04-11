dc1bf462ca31569ccd69451465ee026105ce2963
unknown <p.barbosa93@gmail.com>
2022-04-11 14:03:04 -0500

    created loading to wait for the info to arrive

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import Loading from '../components/Loading'
 
 function ProductDetails() {
   const product = useSelector(state => state.product)
-  console.log(product)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
   return (
     <div>
-      <h1>Product Details</h1>
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <p>{product.price}</p>
+      {loading ? <Loading /> : < div >
+        <h1>Product Details</h1>
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+      </div >}
     </div>
   )
 }
